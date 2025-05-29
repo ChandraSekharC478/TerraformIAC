012c46ff727845f2ca887b3833da4ef159360677
ChandraSekhar <Chandrasekharc784@gmail.com>
2025-05-29 21:56:55 +0530

    Update backend.tf

--- Day-4/backend.tf
+++ Day-4/backend.tf
@@ -1,9 +1,9 @@
-# terraform {
-#  backend "s3" {
-#    bucket         = "chandhu-bucket-remote-backend-xyz"
-#    key            = "chandra/terraform.tfstate"
-#    region         = "us-east-1"
-#   #  dynamodb_table = "terraform_locks"
+terraform {
+  backend "s3" {
+    bucket         = "chandhu-bucket-remote-backend-xyz"
+    key            = "chandra/terraform.tfstate"
+    region         = "us-east-1"
+   dynamodb_table = "terraform_locks"
    
-#  }
-# }
+}
+}
